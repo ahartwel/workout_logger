b4ed288d2de357b8b5fe13337fc4f65dc9c8a323
ahartwel <hartwellalex@gmail.com>
2017-01-04 11:00:20 -0500

    working on setup script

--- setup.sh
+++ setup.sh
@@ -18,27 +18,25 @@ exit
 else
 echo "Ok, coolio, the bundle id is $BUNDLEID"
 fi
-printf "What is the google analytics tracking id? -> "
-read TRACKINGID
-printf "What is the google analytics project id? -> "
-read PROJECTID
-printf "What is the google app id? ->"
-read GOOGLEAPPID
+#printf "What is the google analytics tracking id? -> "
+#read TRACKINGID
+#printf "What is the google analytics project id? -> "
+#read PROJECTID
+#printf "What is the google app id? ->"
+#read GOOGLEAPPID
 
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+find -name "TestApp*.*" -exec rename 's/^$NAME//' {} ";"
+for file in $(grep -l -R "TestApp" $DIR)
+  do
+      echo `LANG=C LC_ALL=C sed -i '' "s/TestApp/$NAME/g" $file > tmpfile.tmp`
+  let i++;
+       echo "Modiefied: " $file
+  done
 
 for file in $(find . -name 'SwarmApp*');
-do
-echo mv "$file" #"${file/MP4./}"
-done
-
-
-exit
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#find -name "TestApp*.*" -exec rename 's/^$NAME//' {} ";"
-#for file in $(grep -l -R "TestApp" $DIR)
-#  do
-#      LANG=C LC_ALL=C sed -i '' "s/TestApp/$NAME/g" $file > tmpfile.tmp
-#  let i++;
-#       echo "Modiefied: " $file
-#  done
-#find -name "TestApp*.*" -exec rename 's/^$NAME//' {} ";" 
+    do
+    RENAME=`echo "$file" | sed -e "s/SwarmApp/${NAME}/g"`
+    echo mv "$file" --- "$RENAME"
+    done
